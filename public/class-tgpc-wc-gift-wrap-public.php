24e89f15a61548ac27a15bd002dac9ed8ff464f4
pexlechris <pexlechris@gmail.com>
2022-01-05 19:16:51 +0200

    Change icon in frontend and filters has been added

--- public/class-tgpc-wc-gift-wrap-public.php
+++ public/class-tgpc-wc-gift-wrap-public.php
@@ -77,20 +77,56 @@ class Tgpc_Wc_Gift_Wrap_Public {
 	public function tgpc_add_gift_checkbox_on_checkout( $checkout ) {
 
 		$label_text = esc_html__('Gift wrapper', 'tgpc-wc-gift-wrap' );
-		$label_icon = '';
+		$label_text = '<span class="tgpc_enable_checkout_gift_wrapper--label_text">' . $label_text . '</span>';
 
-		$gift_icon_url = apply_filters( 'tgpc_wc_gift_wrapper_icon', trailingslashit( TGPC_WC_GIFT_WRAP_PLUGIN_DIR_URL ) . 'assets/gift-outline.svg' );
-		$inline_style  = 'style="display: inline-block; vertical-align: text-bottom; margin-right: 4px;"';
+		$inline_style  = 'width: 17px; height: 17px; display: inline-block; vertical-align: text-bottom; margin-right: 4px;';
+		$class = 'tgpc_enable_checkout_gift_wrapper--label_icon';
 
-		if ( !empty( $gift_icon_url ) ) {
-			$label_icon = '<img src="' . esc_url( $gift_icon_url ) . '" class="tgpc-gift-box-icon" ' . $inline_style . ' alt="'. esc_html__( 'Gift wrapper selected', 'tgpc-wc-gift-wrap' ).'" width="17" height="17" >';
+
+
+		//$gift_icon_url = apply_filters( 'tgpc_wc_gift_wrapper_icon_url', trailingslashit( TGPC_WC_GIFT_WRAP_PLUGIN_DIR_URL ) . 'assets/gift-outline.svg' );
+
+		$gift_icon_url = '';
+
+		/**
+		 * Gift wrapper icon's url.
+		 *
+		 * This filter determines the url of the icon that will be used.
+		 * If is empty, the default svg icon will be used.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param string $gift_icon_url The gift icon's url. Default is empty.
+		 */
+		$gift_icon_url = apply_filters( 'tgpc_wc_gift_wrapper_icon_url', $gift_icon_url);
+
+		if ( empty( $gift_icon_url ) ) {
+			$label_icon = '<svg class="' . $class . '" style="' . $inline_style . '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M22,12V20A2,2 0 0,1 20,22H4A2,2 0 0,1 2,20V12A1,1 0 0,1 1,11V8A2,2 0 0,1 3,6H6.17C6.06,5.69 6,5.35 6,5A3,3 0 0,1 9,2C10,2 10.88,2.5 11.43,3.24V3.23L12,4L12.57,3.23V3.24C13.12,2.5 14,2 15,2A3,3 0 0,1 18,5C18,5.35 17.94,5.69 17.83,6H21A2,2 0 0,1 23,8V11A1,1 0 0,1 22,12M4,20H11V12H4V20M20,20V12H13V20H20M9,4A1,1 0 0,0 8,5A1,1 0 0,0 9,6A1,1 0 0,0 10,5A1,1 0 0,0 9,4M15,4A1,1 0 0,0 14,5A1,1 0 0,0 15,6A1,1 0 0,0 16,5A1,1 0 0,0 15,4M3,8V10H11V8H3M13,8V10H21V8H13Z" /></svg>';
+		} else {
+			$label_icon = '<img src="' . esc_url( $gift_icon_url ) . '" class="' . $class . '" style="' . $inline_style . '" alt="" >';
 		}
 
-        $checkbox_state = WC()->session->get( 'tgpc_gw_enabled', 0 );
+		/**
+		 * Gift wrapper icon's html.
+		 *
+		 * With this filter, you can alter the html of the gift wrapper icon.
+		 * If $gift_icon_url is not set, svg tag is the icon.
+		 * If $gift_icon_url is set, img tag is the icon.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param string $label_icon The html of the icon.
+		 * @param string $class The class of the html tag, svg or img.
+		 * @param string $inline_style The inline style of the icon.
+		 */
+		$label_icon = apply_filters( 'tgpc_wc_gift_wrapper_icon_html', $label_icon, $class, $inline_style);
+
+
+		$checkbox_state = WC()->session->get( 'tgpc_gw_enabled', 0 );
 
         woocommerce_form_field( 'tgpc_enable_checkout_gift_wrapper', [
 			'type'          => 'checkbox',
-			'label'         => $label_icon . $label_text,
+			'label'         => $label_icon . $label_text, //TODO: Αυτό να το βάλουμε φίλτρο? Στείλε μου στο messenger
 			'required'      => false,
 			'class'         => [ 'form-row-wide', 'update_totals_on_change' ],
 		], $checkbox_state );
