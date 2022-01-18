8d40c0f5ad2e6877446a091e87e47ccc2ed3a819
pexlechris <pexlechris@gmail.com>
2022-01-18 22:33:25 +0200

    - add to WC session variable tgpc_gw_name - Change structure (array) to meta data _tgpc_gift_wrapper, to save fee cost & fee name - Add some PHPDoc

--- admin/class-tgpc-wc-gift-wrap-admin.php
+++ admin/class-tgpc-wc-gift-wrap-admin.php
@@ -66,16 +66,53 @@ class Tgpc_Wc_Gift_Wrap_Admin {
 
         if ( 'order_number' === $column ) {
 
-			$gift_selected = $the_order->get_meta( '_tgpc_gift_wrapper_selected' );
+            if ( $the_order->meta_exists( '_tgpc_gift_wrapper' ) ) {
 
-            if ( 'yes' == $gift_selected ) {
+				$inline_style  = 'display: inline-block; vertical-align: text-bottom; margin-right: 4px;';
+				$width = '17px';
+				$height = '17px';
+				$img_class = 'tgpc-order-list-gift-wrapper--label_icon';
 
-                $gift_icon_url = apply_filters( 'tgpc_wc_gift_wrapper_icon_order_list', trailingslashit( TGPC_WC_GIFT_WRAP_PLUGIN_DIR_URL ) . 'assets/gift-outline.svg' );
-                $inline_style = 'style="margin-left:4px; padding:3px; display: inline-block; vertical-align: top;"';
 
-                if ( !empty( $gift_icon_url ) ) {
-                    echo '<img src="' . esc_url( $gift_icon_url ) . '" class="tgpc-gift-box-icon-admin" ' . $inline_style . ' alt="'. esc_html__( 'Gift wrapper selected', 'tgpc-wc-gift-wrap' ).'" width="17" height="17" >';
-                }
+				$gift_icon_url = '';
+
+				/**
+				 * Gift wrapper icon's url.
+				 *
+				 * This filter determines the url of the icon that will be used.
+				 * If is empty, the default svg icon will be used.
+				 * This filter will be also used for gift wrapper in checkout.
+				 *
+				 * @since 1.0
+				 *
+				 * @param string $gift_icon_url The gift icon's url. Default is empty.
+				 */
+				$gift_icon_url = apply_filters( 'tgpc_wc_gift_wrapper_icon_url', $gift_icon_url);
+
+				if ( empty( $gift_icon_url ) ) {
+					$label_icon = '<svg class="' . $img_class . '" style="' . $inline_style . '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="' . $width . '" height="' . $height . '" viewBox="0 0 24 24"><path d="M22,12V20A2,2 0 0,1 20,22H4A2,2 0 0,1 2,20V12A1,1 0 0,1 1,11V8A2,2 0 0,1 3,6H6.17C6.06,5.69 6,5.35 6,5A3,3 0 0,1 9,2C10,2 10.88,2.5 11.43,3.24V3.23L12,4L12.57,3.23V3.24C13.12,2.5 14,2 15,2A3,3 0 0,1 18,5C18,5.35 17.94,5.69 17.83,6H21A2,2 0 0,1 23,8V11A1,1 0 0,1 22,12M4,20H11V12H4V20M20,20V12H13V20H20M9,4A1,1 0 0,0 8,5A1,1 0 0,0 9,6A1,1 0 0,0 10,5A1,1 0 0,0 9,4M15,4A1,1 0 0,0 14,5A1,1 0 0,0 15,6A1,1 0 0,0 16,5A1,1 0 0,0 15,4M3,8V10H11V8H3M13,8V10H21V8H13Z" /></svg>';
+				} else {
+					$label_icon = '<img src="' . esc_url( $gift_icon_url ) . '" class="' . $img_class . '" style="' . $inline_style . '" width="' . $width . '" height="' . $height . '" alt="" >';
+				}
+
+				/**
+				 * Gift wrapper icon's html.
+				 *
+				 * With this filter, you can alter the html of the gift wrapper icon.
+				 * If $gift_icon_url is not set, svg tag is the icon.
+				 * If $gift_icon_url is set, img tag is the icon.
+				 * This filter will be also used for gift wrapper in checkout.
+				 *
+				 * @since 1.0
+				 *
+				 * @param string $label_icon The html of the icon.
+				 * @param string $img_class The class of the html tag, svg or img.
+				 * @param string $width The width the html tag, svg or img.
+				 * @param string $height The height of the html tag, svg or img.
+				 * @param string $inline_style The inline style of the icon.
+				 */
+				$label_icon = apply_filters( 'tgpc_wc_gift_wrapper_icon_html', $label_icon, $img_class, $width, $height, $inline_style );
+
             }
         }
     }
--- public/class-tgpc-wc-gift-wrap-public.php
+++ public/class-tgpc-wc-gift-wrap-public.php
@@ -64,8 +64,9 @@ class Tgpc_Wc_Gift_Wrap_Public {
 		 *
 		 * This filter determines the url of the icon that will be used.
 		 * If is empty, the default svg icon will be used.
+		 * This filter will be also used for gift wrapper in order list in Dashboard.
 		 *
-		 * @since 1.0.0
+		 * @since 1.0
 		 *
 		 * @param string $gift_icon_url The gift icon's url. Default is empty.
 		 */
@@ -84,7 +85,7 @@ class Tgpc_Wc_Gift_Wrap_Public {
 		 * If $gift_icon_url is not set, svg tag is the icon.
 		 * If $gift_icon_url is set, img tag is the icon.
 		 *
-		 * @since 1.0.0
+		 * @since 1.0
 		 *
 		 * @param string $label_icon The html of the icon.
 		 * @param string $img_class The class of the html tag, svg or img.
@@ -103,14 +104,20 @@ class Tgpc_Wc_Gift_Wrap_Public {
 		 *
 		 * With this filter, you can alter the label.
 		 *
-		 * @since 1.0.0
+		 * @since 1.0
 		 *
 		 * @param string $label The input label as html.
 		 * @param string $label_text The escaped text of the label.
 		 */
 		$label = apply_filters( 'tgpc_wc_gift_wrapper_checkout_label', $label, $label_icon, $label_text );
 
-		$checkbox_state = WC()->session->get( 'tgpc_gw_enabled', 0 );
+		$saved_cost = WC()->session->get( 'tgpc_gw_cost', false );
+
+		if( false === $saved_cost ){
+			$checkbox_state = 0;
+		}else{
+			$checkbox_state = 1;
+		}
 
 		woocommerce_form_field( 'tgpc_enable_checkout_gift_wrapper', [
 			'type'          => 'checkbox',
@@ -140,30 +147,71 @@ class Tgpc_Wc_Gift_Wrap_Public {
 		if ( !empty( $post_data[ 'tgpc_enable_checkout_gift_wrapper' ] )
 			|| !empty( $_POST[ 'tgpc_enable_checkout_gift_wrapper' ] ) ) {
 
+			$fee_name = esc_html__( 'Gift wrapper', 'tgpc-wc-gift-wrap' );
 			$fee_cost   = (float) get_option( 'tgpc_gift_wrapper_cost' );
 			$is_taxable = 'yes' === get_option( 'tgpc_gift_wrapper_cost_tax_status' );
 			$tax_class  = get_option( 'tgpc_gift_wrapper_tax_class', '' );
 
-			WC()->cart->add_fee( esc_html__( 'Gift wrapper', 'tgpc-wc-gift-wrap' ), $fee_cost, $is_taxable, $tax_class );
+			WC()->cart->add_fee( $fee_name, $fee_cost, $is_taxable, $tax_class );
 
-            WC()->session->set( 'tgpc_gw_enabled', 1 );
+			WC()->session->set( 'tgpc_gw_cost', $fee_cost );
+			WC()->session->set( 'tgpc_gw_name', $fee_name );
 		}
         else {
-            WC()->session->set( 'tgpc_gw_enabled', 0 );
+            WC()->session->set( 'tgpc_gw_cost', false );
         }
 	}
 
     /**
      * Save checkbox value to order data.
+	 *
+	 * @since 1.0
      *
-     * @param $order WC_Order
-     * @since 1.0
+     * @param object $order WC_Order
      */
-	public function tgpc_save_gift_box_option_to_order( $order ){
+	public function tgpc_save_gift_wrapper_option_to_order_meta( $order ){
 
 		if ( !empty( $_POST['tgpc_enable_checkout_gift_wrapper'] ) ) {
-			$order->add_meta_data( '_tgpc_gift_wrapper_selected', 'yes' );
+			$cost = WC()->session->get( 'tgpc_gw_cost');
+			$name = WC()->session->get( 'tgpc_gw_name');
+			$order->add_meta_data( '_tgpc_gift_wrapper', array('cost' => $cost, 'name' =>  $name) );
 		}
+	}
+
+	/**
+	 * Hook that enables the display fee in emails.
+	 *
+	 * By default, when the fee cost is 0, the cart fee is not displayed.
+	 * With this hook, we allow to display the gift wrapper fee in the emails,
+	 * using the hook `woocommerce_get_order_item_totals_excl_free_fees`.
+	 *
+	 * @since 1.0
+	 *
+	 * @param array $fees
+	 * @param object $order WC_Order
+	 * @param array $types
+	 * @return array The fees variable that isn't changed.
+	 */
+	public function tgpc_show_fee_even_if_gift_wrapper_cost_is_0($fees, $order, $types) {
+		if ( in_array('fee', $types ) ) {
+			$tgpc_gift_wrapper = $order->get_meta('_tgpc_gift_wrapper');
+			$saved_cost = $tgpc_gift_wrapper['cost'];
+			$saved_name = $tgpc_gift_wrapper['name'];
+
+			foreach($fees as $fee_id => $fee){
+				if( 0 == $saved_cost && $saved_name == $fee->get_name() ){
+					add_filter( 'woocommerce_get_order_item_totals_excl_free_fees', function($bool, $id) use($fee_id){
+						if( $id == $fee_id ){
+							$bool = false;
+						}
+						return $bool;
+					}, 10, 2);
+					break;
+				}
+			}
+
+		}
+		return $fees;
 	}
 
 
