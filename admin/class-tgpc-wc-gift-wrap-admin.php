252810a8e0eb2aeb20409556737e73235c392d87
pexlechris <pexlechris@gmail.com>
2022-01-18 23:16:43 +0200

    - php fixes - phpDoc enhancements

--- admin/class-tgpc-wc-gift-wrap-admin.php
+++ admin/class-tgpc-wc-gift-wrap-admin.php
@@ -68,7 +68,7 @@ class Tgpc_Wc_Gift_Wrap_Admin {
 
             if ( $the_order->meta_exists( '_tgpc_gift_wrapper' ) ) {
 
-				$inline_style  = 'display: inline-block; vertical-align: text-bottom; margin-right: 4px;';
+				$inline_style  = 'display: inline-block; vertical-align: text-bottom; margin-left: 4px;';
 				$width = '17px';
 				$height = '17px';
 				$img_class = 'tgpc-order-list-gift-wrapper--label_icon';
@@ -90,7 +90,7 @@ class Tgpc_Wc_Gift_Wrap_Admin {
 				$gift_icon_url = apply_filters( 'tgpc_wc_gift_wrapper_icon_url', $gift_icon_url);
 
 				if ( empty( $gift_icon_url ) ) {
-					$label_icon = '<svg class="' . $img_class . '" style="' . $inline_style . '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="' . $width . '" height="' . $height . '" viewBox="0 0 24 24"><path d="M22,12V20A2,2 0 0,1 20,22H4A2,2 0 0,1 2,20V12A1,1 0 0,1 1,11V8A2,2 0 0,1 3,6H6.17C6.06,5.69 6,5.35 6,5A3,3 0 0,1 9,2C10,2 10.88,2.5 11.43,3.24V3.23L12,4L12.57,3.23V3.24C13.12,2.5 14,2 15,2A3,3 0 0,1 18,5C18,5.35 17.94,5.69 17.83,6H21A2,2 0 0,1 23,8V11A1,1 0 0,1 22,12M4,20H11V12H4V20M20,20V12H13V20H20M9,4A1,1 0 0,0 8,5A1,1 0 0,0 9,6A1,1 0 0,0 10,5A1,1 0 0,0 9,4M15,4A1,1 0 0,0 14,5A1,1 0 0,0 15,6A1,1 0 0,0 16,5A1,1 0 0,0 15,4M3,8V10H11V8H3M13,8V10H21V8H13Z" /></svg>';
+					$label_icon = '<svg class="' . $img_class . '" width="' . $width . '" height="' . $height . '" style="' . $inline_style . '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 24 24"><path d="M22,12V20A2,2 0 0,1 20,22H4A2,2 0 0,1 2,20V12A1,1 0 0,1 1,11V8A2,2 0 0,1 3,6H6.17C6.06,5.69 6,5.35 6,5A3,3 0 0,1 9,2C10,2 10.88,2.5 11.43,3.24V3.23L12,4L12.57,3.23V3.24C13.12,2.5 14,2 15,2A3,3 0 0,1 18,5C18,5.35 17.94,5.69 17.83,6H21A2,2 0 0,1 23,8V11A1,1 0 0,1 22,12M4,20H11V12H4V20M20,20V12H13V20H20M9,4A1,1 0 0,0 8,5A1,1 0 0,0 9,6A1,1 0 0,0 10,5A1,1 0 0,0 9,4M15,4A1,1 0 0,0 14,5A1,1 0 0,0 15,6A1,1 0 0,0 16,5A1,1 0 0,0 15,4M3,8V10H11V8H3M13,8V10H21V8H13Z" /></svg>';
 				} else {
 					$label_icon = '<img src="' . esc_url( $gift_icon_url ) . '" class="' . $img_class . '" style="' . $inline_style . '" width="' . $width . '" height="' . $height . '" alt="" >';
 				}
@@ -99,9 +99,11 @@ class Tgpc_Wc_Gift_Wrap_Admin {
 				 * Gift wrapper icon's html.
 				 *
 				 * With this filter, you can alter the html of the gift wrapper icon.
-				 * If $gift_icon_url is not set, svg tag is the icon.
-				 * If $gift_icon_url is set, img tag is the icon.
 				 * This filter will be also used for gift wrapper in checkout.
+				 * USE CASES
+				 * - If $gift_icon_url is not set above, the icon is <svg>.
+				 * - If $gift_icon_url is set above, the icon is <img>.
+				 * - if $label_icon is an empty string, icon isn't printed
 				 *
 				 * @since 1.0
 				 *
@@ -112,6 +114,10 @@ class Tgpc_Wc_Gift_Wrap_Admin {
 				 * @param string $inline_style The inline style of the icon.
 				 */
 				$label_icon = apply_filters( 'tgpc_wc_gift_wrapper_icon_html', $label_icon, $img_class, $width, $height, $inline_style );
+
+				if ( !empty( $label_icon ) ) {
+					echo $label_icon;
+				}
 
             }
         }
--- public/class-tgpc-wc-gift-wrap-public.php
+++ public/class-tgpc-wc-gift-wrap-public.php
@@ -73,7 +73,7 @@ class Tgpc_Wc_Gift_Wrap_Public {
 		$gift_icon_url = apply_filters( 'tgpc_wc_gift_wrapper_icon_url', $gift_icon_url);
 
 		if ( empty( $gift_icon_url ) ) {
-			$label_icon = '<svg class="' . $img_class . '" style="' . $inline_style . '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="' . $width . '" height="' . $height . '" viewBox="0 0 24 24"><path d="M22,12V20A2,2 0 0,1 20,22H4A2,2 0 0,1 2,20V12A1,1 0 0,1 1,11V8A2,2 0 0,1 3,6H6.17C6.06,5.69 6,5.35 6,5A3,3 0 0,1 9,2C10,2 10.88,2.5 11.43,3.24V3.23L12,4L12.57,3.23V3.24C13.12,2.5 14,2 15,2A3,3 0 0,1 18,5C18,5.35 17.94,5.69 17.83,6H21A2,2 0 0,1 23,8V11A1,1 0 0,1 22,12M4,20H11V12H4V20M20,20V12H13V20H20M9,4A1,1 0 0,0 8,5A1,1 0 0,0 9,6A1,1 0 0,0 10,5A1,1 0 0,0 9,4M15,4A1,1 0 0,0 14,5A1,1 0 0,0 15,6A1,1 0 0,0 16,5A1,1 0 0,0 15,4M3,8V10H11V8H3M13,8V10H21V8H13Z" /></svg>';
+			$label_icon = '<svg class="' . $img_class . '" width="' . $width . '" height="' . $height . '" style="' . $inline_style . '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 24 24"><path d="M22,12V20A2,2 0 0,1 20,22H4A2,2 0 0,1 2,20V12A1,1 0 0,1 1,11V8A2,2 0 0,1 3,6H6.17C6.06,5.69 6,5.35 6,5A3,3 0 0,1 9,2C10,2 10.88,2.5 11.43,3.24V3.23L12,4L12.57,3.23V3.24C13.12,2.5 14,2 15,2A3,3 0 0,1 18,5C18,5.35 17.94,5.69 17.83,6H21A2,2 0 0,1 23,8V11A1,1 0 0,1 22,12M4,20H11V12H4V20M20,20V12H13V20H20M9,4A1,1 0 0,0 8,5A1,1 0 0,0 9,6A1,1 0 0,0 10,5A1,1 0 0,0 9,4M15,4A1,1 0 0,0 14,5A1,1 0 0,0 15,6A1,1 0 0,0 16,5A1,1 0 0,0 15,4M3,8V10H11V8H3M13,8V10H21V8H13Z" /></svg>';
 		} else {
 			$label_icon = '<img src="' . esc_url( $gift_icon_url ) . '" class="' . $img_class . '" style="' . $inline_style . '" width="' . $width . '" height="' . $height . '" alt="" >';
 		}
@@ -82,8 +82,11 @@ class Tgpc_Wc_Gift_Wrap_Public {
 		 * Gift wrapper icon's html.
 		 *
 		 * With this filter, you can alter the html of the gift wrapper icon.
-		 * If $gift_icon_url is not set, svg tag is the icon.
-		 * If $gift_icon_url is set, img tag is the icon.
+		 * This filter will be also used for gift wrapper in order list in Dashboard.
+		 * USE CASES
+		 * - If $gift_icon_url is not set above, the icon is <svg>.
+		 * - If $gift_icon_url is set above, the icon is <img>.
+		 * - if $label_icon is an empty string, icon isn't printed
 		 *
 		 * @since 1.0
 		 *
